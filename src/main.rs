eeac77d4af2d888b364965fc6526697c99518c15
Kenton Hamaluik <kenton@hamaluik.ca>
2018-10-20 14:43:34 -0600

    load graph from a json file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,70 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 extern crate pathfinding;
 use pathfinding::prelude::astar;
 use std::u64;
 use std::f64;
 use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize)]
+struct Node {
+    id: u64,
+    lat: f64,
+    lon: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Edge {
+    from: u64,
+    to: u64,
+    cost: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MapGraph {
+    nodes: Vec<Node>,
+    edges: Vec<Edge>,
+}
+
+fn load_raw_graph<P: AsRef<Path>>(path: P) -> Result<MapGraph, Box<Error>> {
+    let file = File::open(path)?;
+    let g = serde_json::from_reader(file)?;
+    Ok(g)
+}
+
+fn transform_raw_graph(g: &MapGraph) -> (HashMap<u64, (f64, f64)>, HashMap<u64, HashMap<u64, f64>>) {
+    let mut nodes: HashMap<u64, (f64, f64)> = HashMap::new();
+    for node in g.nodes.iter() {
+        nodes.insert(node.id, (node.lat, node.lon));
+    }
+
+    let mut edges: HashMap<u64, HashMap<u64, f64>> = HashMap::new();
+    for edge in g.edges.iter() {
+        if !edges.contains_key(&edge.from) {
+            edges.insert(edge.from, HashMap::new());
+        }
+        edges.get_mut(&edge.from).unwrap().insert(edge.to, edge.cost);
+    }
+
+    (nodes, edges)
+}
 
 fn main() {
-    let mut nodes: HashMap<u64, (f64, f64)> = HashMap::new();
+    let g: MapGraph = match load_raw_graph("map.json") {
+        Err(e) => {
+            eprintln!("Failed to load map: {:?}", e);
+            return;
+        },
+        Ok(g) => g,
+    };
+    let (nodes, edges) = transform_raw_graph(&g);
+
+    /*let mut nodes: HashMap<u64, (f64, f64)> = HashMap::new();
     nodes.insert(42, (0.0, 0.0));
     nodes.insert(1, (0.5, -0.25));
     nodes.insert(22, (0.5, 0.0));
@@ -24,7 +83,7 @@ fn main() {
 
     let mut edge: HashMap<u64, f64> = HashMap::new();
     edge.insert(3, 0.5);
-    edges.insert(22, edge);
+    edges.insert(22, edge);*/
 
     println!("Nodes:");
     for i in vec![42, 1, 22, 3] {
