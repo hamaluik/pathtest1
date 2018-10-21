409e2475244783a36794f80f842bffae45aabb21
Kenton Hamaluik <kenton@hamaluik.ca>
2018-10-20 18:29:44 -0600

    removed extraneous stuff

--- src/main.rs
+++ src/main.rs
@@ -64,47 +64,6 @@ fn main() {
     };
     let (nodes, edges) = transform_raw_graph(&g);
 
-    /*let mut nodes: HashMap<u64, (f64, f64)> = HashMap::new();
-    nodes.insert(42, (0.0, 0.0));
-    nodes.insert(1, (0.5, -0.25));
-    nodes.insert(22, (0.5, 0.0));
-    nodes.insert(3, (1.0, 0.0));
-
-    let mut edges: HashMap<u64, HashMap<u64, f64>> = HashMap::new();
-
-    let mut edge: HashMap<u64, f64> = HashMap::new();
-    edge.insert(1, 0.5);
-    edge.insert(22, 1.5);
-    edges.insert(42, edge);
-
-    let mut edge: HashMap<u64, f64> = HashMap::new();
-    edge.insert(3, 0.5);
-    edges.insert(1, edge);
-
-    let mut edge: HashMap<u64, f64> = HashMap::new();
-    edge.insert(3, 0.5);
-    edges.insert(22, edge);*/
-
-    println!("Nodes:");
-    for i in vec![42, 1, 22, 3] {
-        let derp = match nodes.get(&i) {
-            None => (f64::NAN, f64::NAN),
-            Some(v) => *v,
-        };
-        println!("{} => {:?}", i, derp);
-    }
-
-    println!("Edges:");
-    for i in vec![42, 1, 22, 3] {
-        let derp: Vec<(u64, u64)> = match edges.get(&i) {
-            None => Vec::new(),
-            Some(e) => {
-                e.iter().map(|n| (*n.0, (n.1 * 100.0) as u64)).collect()
-            },
-        };
-        println!("{} => {:?}", i, derp);
-    }
-
     let target_node: u64 = 3;
     let result = astar(
         &42u64,
